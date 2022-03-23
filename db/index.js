428c5ce940df18082cf97beefca20c3fe360c4aa
Lyere <BPpearsall@gmail.com>
2022-03-23 10:04:09 -0400

    insatlled npm and ignored thme

--- db/index.js
+++ db/index.js
@@ -1,108 +1,315 @@
-const connection = require("./connection");
+const { prompt } = require("inquirer");
+const logo = require("asciiart-logo");
+const db = require("./db");
+require("console.table");
 
-class DB {
-  // Keeping a reference to the connection on the class in case we need it later
-  constructor(connection) {
-    this.connection = connection;
-  }
+init();
 
-  // Find all employees, join with roles and departments to display their roles, salaries, departments, and managers
-  findAllEmployees() {
-    return this.connection.query(
-      // CREATE SELECT STATMENT WITH THE FOLLOWING COLUMNS FROM THREE TABLES.
-      // id, first_name, last_name FROM employee TABLE AND department name from department TABLE AND SELECT salary FROM role TABLE
-      // YOUR NEED TO USE LEFT JOINS TO JOIN THREE TABLES
-      // TODO: YOUR CODE HERE
+// Display logo text, load main prompts
+function init() {
+  const logoText = logo({ name: "Employee Manager" }).render();
 
-    );
-  }
+  console.log(logoText);
 
-  // Find all employees except the given employee id
-  findAllPossibleManagers(employeeId) {
-    return this.connection.query(
-      "SELECT id, first_name, last_name FROM employee WHERE id != ?",
-      employeeId
-    );
-  }
+  loadMainPrompts();
+}
 
-  // Create a new employee
-  createEmployee(employee) {
-    return this.connection.query("INSERT INTO employee SET ?", employee);
-  }
+async function loadMainPrompts() {
+  const { choice } = await prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "What would you like to do?",
+      choices: [
+        {
+          name: "View All Employees",
+          value: "VIEW_EMPLOYEES"
+        },
+        {
+          name: "View All Employees By Department",
+          value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
+        },
+        // Bonus
+        // {
+        //   name: "View All Employees By Manager",
+        //   value: "VIEW_EMPLOYEES_BY_MANAGER"
+        // },
+        {
+          name: "Add Employee",
+          value: "ADD_EMPLOYEE"
+        },
+        // Bonus
+        // {
+        //   name: "Remove Employee",
+        //   value: "REMOVE_EMPLOYEE"
+        // },
+        {
+          name: "Update Employee Role",
+          value: "UPDATE_EMPLOYEE_ROLE"
+        },
+        // Bonus
+        // {
+        //   name: "Update Employee Manager",
+        //   value: "UPDATE_EMPLOYEE_MANAGER"
+        // },
+        {
+          name: "View All Roles",
+          value: "VIEW_ROLES"
+        },
+        {
+          name: "Add Role",
+          value: "ADD_ROLE"
+        },
+        //  Bonus
+        // {
+        //   name: "Remove Role",
+        //   value: "REMOVE_ROLE"
+        // },
+        {
+          name: "View All Departments",
+          value: "VIEW_DEPARTMENTS"
+        },
+        {
+          name: "Add Department",
+          value: "ADD_DEPARTMENT"
+        },
+        //  Bonus
+        // {
+        //   name: "Remove Department",
+        //   value: "REMOVE_DEPARTMENT"
+        // },
+        {
+          name: "Quit",
+          value: "QUIT"
+        }
+      ]
+    }
+  ]);
 
-
-  // Update the given employee's role
-  updateEmployeeRole(employeeId, roleId) {
-    return this.connection.query(
-      // TODO: YOUR CODE HERE
-
-    );
-  }
-
-  // Update the given employee's manager
-  updateEmployeeManager(employeeId, managerId) {
-    return this.connection.query(
-      "UPDATE employee SET manager_id = ? WHERE id = ?",
-      [managerId, employeeId]
-    );
-  }
-
-  // Find all roles, join with departments to display the department name
-  findAllRoles() {
-    return this.connection.query(
-      // SELECT THE FOLLOWING COLUMNS:
-      // id, title, salary FROM role TABLE AND department name FROM department TABLE
-      // YOU NEED TO USE LEFT JOIN TO JOIN role and department TABLES
-      // TODO: YOUR CODE HERE
-
-    );
-  }
-
-  // Create a new role
-  createRole(role) {
-    return this.connection.query(
-      // TODO: YOUR CODE HERE
-
-      );
-  }
-
-
-  // Find all departments, join with employees and roles and sum up utilized department budget
-  findAllDepartments() {
-    return this.connection.query(
-      "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget \
-      FROM department LEFT JOIN role ON role.department_id = department.id \
-      LEFT JOIN employee ON employee.role_id = role.id \
-      GROUP BY department.id, department.name"
-    );
-  }
-
-  // Create a new department
-  createDepartment(department) {
-    return this.connection.query(
-      // TODO: YOUR CODE HERE
-    );
-  }
-
-  // Find all employees in a given department, join with roles to display role titles
-  findAllEmployeesByDepartment(departmentId) {
-    return this.connection.query(
-      "SELECT employee.id, employee.first_name, employee.last_name, role.title \
-      FROM employee \
-      LEFT JOIN role on employee.role_id = role.id \
-      LEFT JOIN department department on role.department_id = department.id \
-      WHERE department.id = ?;",
-      departmentId
-    );
-  }
-
-  // Find all employees by manager, join with departments and roles to display titles and department names
-  findAllEmployeesByManager(managerId) {
-    return this.connection.query(
-      // TODO: YOUR CODE HERE
-
-    );
+  // Call the appropriate function depending on what the user chose
+  switch (choice) {
+    case "VIEW_EMPLOYEES":
+      return viewEmployees();
+    case "VIEW_EMPLOYEES_BY_DEPARTMENT":
+      return viewEmployeesByDepartment();
+    case "ADD_EMPLOYEE":
+      return addEmployee();
+    case "UPDATE_EMPLOYEE_ROLE":
+      return updateEmployeeRole();
+    case "VIEW_DEPARTMENTS":
+      return viewDepartments();
+    case "ADD_DEPARTMENT":
+      return addDepartment();
+    case "VIEW_ROLES":
+      return viewRoles();
+    case "ADD_ROLE":
+      return addRole();
+    default:
+      return quit();
   }
 }
 
-module.exports = new DB(connection);
+async function viewEmployees() {
+  const employees = await db.findAllEmployees();
+
+  console.log("\n");
+  console.table(employees);
+
+  loadMainPrompts();
+}
+
+async function viewEmployeesByDepartment() {
+  const departments = await db.findAllDepartments();
+
+  const departmentChoices = departments.map(({ id, name }) => ({
+    // CREATE TWO PROPERTIES name AND value FOR THIS OBJECT. THE PROPERTY name SHOULD CONTAIN THE NAME OF THE DEPARTMENT.
+    // THE PROPERTY value SHOULD CONTAIN id.
+    // TODO: YOUR CODE HERE
+
+  }));
+
+  const { departmentId } = await prompt([
+    {
+      type: "list",
+      name: "departmentId",
+      message: "Which department would you like to see employees for?",
+      choices: departmentChoices
+    }
+  ]);
+
+  const employees = await db.findAllEmployeesByDepartment(departmentId);
+
+  console.log("\n");
+  console.table(employees);
+
+  loadMainPrompts();
+}
+
+async function updateEmployeeRole() {
+  const employees = await db.findAllEmployees();
+
+  const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
+    // CREATE TWO PROPERTIES name AMD value FOR THIS OBJECT. THE PROPERTY name SHOULD CONTAIN THE CONCATENATION OF THE FIRST HAME AND THE LAST NAME.
+    // THE PROPERTY value SHOULD CONTAIN id.
+    // THIS OBJECT FOR EACH MANAGER WILL RETURN TO MAP() TO CONSTRUCT AN ARRAY TO BE RETURNED AND BE STORED TO managerChoices.
+    // TODO: YOUR CODE HERE
+
+  }));
+
+  const { employeeId } = await prompt([
+    {
+      type: "list",
+      name: "employeeId",
+      message: "Which employee's role do you want to update?",
+      choices: employeeChoices
+    }
+  ]);
+
+  const roles = await db.findAllRoles();
+
+  const roleChoices = roles.map(({ id, title }) => ({
+    name: title,
+    value: id
+  }));
+
+  const { roleId } = await prompt([
+    {
+      type: "list",
+      name: "roleId",
+      message: "Which role do you want to assign the selected employee?",
+      choices: roleChoices
+    }
+  ]);
+
+  await db.updateEmployeeRole(employeeId, roleId);
+
+  console.log("Updated employee's role");
+
+  loadMainPrompts();
+}
+
+async function viewRoles() {
+  const roles = await db.findAllRoles();
+
+  console.log("\n");
+  console.table(roles);
+
+  loadMainPrompts();
+}
+
+async function addRole() {
+  const departments = await db.findAllDepartments();
+
+  const departmentChoices = departments.map(({ id, name }) => ({
+    name: name,
+    value: id
+  }));
+
+  const role = await prompt([
+    {
+      name: "title",
+      message: "What is the name of the role?"
+    },
+    {
+      name: "salary",
+      message: "What is the salary of the role?"
+    },
+    {
+      type: "list",
+      name: "department_id",
+      message: "Which department does the role belong to?",
+      choices: departmentChoices
+    }
+  ]);
+
+  await db.createRole(role);
+
+  console.log(`Added ${role.title} to the database`);
+
+  loadMainPrompts();
+}
+
+async function viewDepartments() {
+  const departments = await db.findAllDepartments();
+
+  console.log("\n");
+  console.table(departments);
+
+  loadMainPrompts();
+}
+
+async function addDepartment() {
+  const department = await prompt([
+    {
+      name: "name",
+      message: "What is the name of the department?"
+    }
+  ]);
+
+  await db.createDepartment(department);
+
+  console.log(`Added ${department.name} to the database`);
+
+  loadMainPrompts();
+}
+
+async function addEmployee() {
+  const roles = await db.findAllRoles();
+  const employees = await db.findAllEmployees();
+
+  const employee = await prompt([
+    {
+      name: "first_name",
+      message: "What is the employee's first name?"
+    },
+    {
+      name: "last_name",
+      message: "What is the employee's last name?"
+    }
+  ]);
+
+  const roleChoices = roles.map(({ id, title }) => ({
+    name: title,
+    value: id
+  }));
+
+  const { roleId } = await prompt({
+    type: "list",
+    name: "roleId",
+    message: "What is the employee's role?",
+    choices: roleChoices
+  });
+
+  employee.role_id = roleId;
+
+  const managerChoices = employees.map(({ id, first_name, last_name }) => ({
+    // CREATE TWO PROPERTIES name AMD value FOR THIS OBJECT. THE PROPERTY name SHOULD CONTAIN THE CONCATENATION OF THE FIRST HAME AND THE LAST NAME.
+    // THE PROPERTY value SHOULD CONTAIN id.
+    // THIS OBJECT FOR EACH MANAGER WILL RETURN TO MAP() TO CONSTRUCT AN ARRAY TO BE RETURNED AND BE STORED TO managerChoices.
+    // TODO: YOUR CODE HERE
+
+  }));
+  managerChoices.unshift({ name: "None", value: null });
+
+  const { managerId } = await prompt({
+    type: "list",
+    name: "managerId",
+    message: "Who is the employee's manager?",
+    choices: managerChoices
+  });
+
+  employee.manager_id = managerId;
+
+  await db.createEmployee(employee);
+
+  console.log(
+    `Added ${employee.first_name} ${employee.last_name} to the database`
+  );
+
+  loadMainPrompts();
+}
+
+function quit() {
+  console.log("Goodbye!");
+  process.exit();
+}
